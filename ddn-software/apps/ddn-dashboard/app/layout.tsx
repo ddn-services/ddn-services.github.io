8574f68134ca8feb17b629cea735ee04d26fc191
Thanh Quang Long <long.thanh-quang@banvien.com.vn>
2024-01-09 17:27:33 +0700

    [Update] remove base

--- ddn-software/apps/ddn-dashboard/app/layout.tsx
+++ ddn-software/apps/ddn-dashboard/app/layout.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { ReduxProvider } from 'apps/ddn-dashboard/redux/provider';
 import './global.css';
 import { StyledComponentsRegistry } from './registry';
@@ -18,8 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           title="DDN Software"
           aria-description="This is the system dashboard using the services provided by DevDaNang systems"
         />
-        <link rel="icon" type="image/x-icon" href="favicon.ico" />
-
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap" rel="stylesheet" />
