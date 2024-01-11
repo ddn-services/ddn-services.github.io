5d45e5d0fce2016b45d9c1f128d18aa07b034df7
Thanh Quang Long <long.thanh-quang@banvien.com.vn>
2024-01-11 11:47:50 +0700

    [Update] update init

--- ddn-software/apps/ddn-dashboard/redux/provider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { AppStore } from './store';
-import { Provider } from 'react-redux';
-import React from 'react';
-
-export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={AppStore}>{children}</Provider>;
-}
